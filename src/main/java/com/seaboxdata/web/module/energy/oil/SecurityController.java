8f5fa8d6285bbe8dae1a040a4ede4acc36f9cfb2
HDLR <492587402@qq.com>
2019-01-09 11:15:59 +0800

    创建目录

---
+++ src/main/java/com/seaboxdata/web/module/energy/oil/SecurityController.java
@@ -0,0 +1,10 @@
+package com.seaboxdata.web.module.energy.oil;
+
+import com.seaboxdata.core.base.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/oil/security")
+public class SecurityController extends BaseController {
+}
