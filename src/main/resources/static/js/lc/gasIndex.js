5ee436e3e2628fadec885621fbe063a676fd63de
dfjxps2 <dfjxps2@seaboxdata.com>
2019-01-15 14:15:45 +0800

    zlc v6.1

--- src/main/resources/static/js/lc/gasIndex.js
+++ src/main/resources/static/js/lc/gasIndex.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	getdata('/lc/gasIndex/chart1.json',chart1);
 	getdatax('/lc/gasIndex/chart2.json',chart2);
-	getdata('/lc/gasIndex/chart3.json',chart3);
-	getdata('/lc/gasIndex/chart4.json',chart4);
-	getdata('/lc/gasIndex/chart5.json',chart5);
+	getdatax('/lc/gasIndex/chart3.json',chart3);
+	getdatax('/lc/gasIndex/chart4.json',chart4);
+	getdatax('/lc/gasIndex/chart5.json',chart5);
 	getdata('/lc/gasIndex/main.json',main);
 });
 function main(data){
@@ -41,8 +41,10 @@ function main(data){
 		}],
 		yAxis: [
 			{
+				name:'亿立方米',
+				nameGap:-1,
 				nameTextStyle:{
-					padding:[0,0,0,45],
+					padding:[0,0,0,65],
 					align:'center',
 					color:'#fff',
 				},
@@ -243,6 +245,229 @@ function chart2(data, ix){
 		legend: {
 			data: data.legend,
 		},
+		color:['#FFD743','#2AB7FF','#30FFFE','#216FFF'],
+		grid: {
+			left: '5%',
+			right:'5%',
+			top:'10%',
+			bottom:'18%',
+			containLabel: true
+		},
+		tooltip: {
+			trigger: 'axis',
+			axisPointer: { // 坐标轴指示器，坐标轴触发有效
+				type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+			}
+		},
+		xAxis: [{
+			type: 'category',
+			gridIndex: 0,
+			data: data.xData,
+			axisLine: {
+				lineStyle: {
+					color: '#38b8ff'
+				}
+			},
+			axisLabel: {
+				color: '#fff',
+				fontSize: 10
+			}
+		}],
+		yAxis: [
+			{
+				name:'',
+				nameGap:-5,
+				nameTextStyle:{
+					padding:[0,0,0,25],
+					align:'center',
+					color:'#fff',
+				},
+				axisLine: {
+					lineStyle: {
+						color: '#38b8ff'
+					}
+				},
+				axisLabel: {
+					color: '#fff',
+					fontSize: 10,
+				},
+				splitLine: {
+					show:false,
+					lineStyle: {
+						color: '#0177d4'
+					}
+				}
+			},
+			{
+				nameTextStyle: {
+					color: '#fff',
+					fontSize: 10
+				},
+				axisLine: {
+					lineStyle: {
+						color: '#38b8ff'
+					}
+				},
+				axisLabel: {
+					color: '#fff',
+					fontSize: 10,
+					formatter: '{value}%'
+				},
+				splitLine: {
+					show:false,
+					lineStyle: {
+						color: '#0177d4'
+					}
+				}
+			}],
+		series: [
+			{
+				name: "总排放量",
+				type: "bar",
+				barWidth: '30%',
+				itemStyle: {
+					normal: {
+					},
+				},
+				data: data.data[ix][0],
+			},
+			{
+				name: "排放系数",
+				type: "bar",
+				barWidth: '30%',
+				itemStyle: {
+					normal: {
+					},
+				},
+				data: data.data[ix][1],
+			},
+			{
+				name: "增长率",
+				type: "line",
+				yAxisIndex:1,
+				itemStyle: {
+					normal: {
+					},
+				},
+				data: data.data[ix][2],
+			},
+			{
+				name: "排放系数增长率",
+				type: "line",
+				yAxisIndex:1,
+				itemStyle: {
+					normal: {
+					},
+				},
+				data: data.data[ix][3],
+			},
+		]
+	};
+	$chart.init('#chart2', option);
+}
+//低碳科技支出与总支出占比
+function chart3(data, ix){
+	var option =  {
+		legend: {
+			data: data.legend,
+		},
+		grid: {
+			left: '5%',
+			right:'5%',
+			top:'10%',
+			bottom:'18%',
+			containLabel: true
+		},
+		tooltip: {
+			trigger: 'axis',
+			axisPointer: { // 坐标轴指示器，坐标轴触发有效
+				type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+			}
+		},
+		xAxis: [{
+			type: 'category',
+			gridIndex: 0,
+			data: data.xData,
+			axisLine: {
+				lineStyle: {
+					color: '#38b8ff'
+				}
+			},
+			axisLabel: {
+				color: '#fff',
+				fontSize: 10
+			}
+		}],
+		yAxis: [
+			{
+				name:'',
+				nameGap:-5,
+				nameTextStyle:{
+					padding:[0,0,0,25],
+					align:'center',
+					color:'#fff',
+				},
+				axisLine: {
+					lineStyle: {
+						color: '#38b8ff'
+					}
+				},
+				axisLabel: {
+					color: '#fff',
+					fontSize: 10,
+				},
+				splitLine: {
+					show:false,
+					lineStyle: {
+						color: '#0177d4'
+					}
+				}
+			},{
+				nameTextStyle: {
+					color: '#fff',
+					fontSize: 10
+				},
+				axisLine: {
+					lineStyle: {
+						color: '#38b8ff'
+					}
+				},
+				axisLabel: {
+					color: '#fff',
+					fontSize: 10,
+					formatter: '{value}%'
+				},
+				splitLine: {
+					show:false,
+					lineStyle: {
+						color: '#0177d4'
+					}
+				}
+			}],
+		color:['#FFD743','#2AB7FF','#30FFFE','#216FFF'],
+		series: [
+			{
+				name: "研发人员与产业人员占比",
+				type: "bar",
+				barWidth: '20%',
+				itemStyle: {
+					normal: {
+						barBorderRadius: 10,
+					},
+				},
+				data: data.data[ix][0],
+			},
+		]
+	};
+	$chart.init('#chart3', option);
+}
+//低碳技术研发人员与产业人员占比
+function chart4(data,ix){
+	var option =  {
+		legend: {
+			data: data.legend,
+		},
+		color:['#FFD743','#2AB7FF','#30FFFE','#216FFF'],
 		grid: {
 			left: '5%',
 			right:'5%',
@@ -318,261 +543,26 @@ function chart2(data, ix){
 			}],
 		series: [
 			{
-				name: "CO2排放量",
+				name: "研发人员与产业人员占比",
 				type: "bar",
-				barWidth: '30%',
+				barWidth: '20%',
 				itemStyle: {
 					normal: {
 						color: '#2c18f3',
+						barBorderRadius: 10,
 					},
 				},
 				data: data.data[ix][0],
 			},
 		]
 	};
-	$chart.init('#chart2', option);
-}
-//低碳科技支出与总支出占比
-function chart3(data){
-	var option =  {
-		legend: {
-			data: data[0],
-		},
-		grid: {
-			left: '5%',
-			right:'5%',
-			top:'10%',
-			bottom:'18%',
-			containLabel: true
-		},
-		tooltip: {
-			trigger: 'axis',
-			axisPointer: { // 坐标轴指示器，坐标轴触发有效
-				type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-			}
-		},
-		xAxis: [{
-			type: 'category',
-			gridIndex: 0,
-			data: data[1],
-			axisLine: {
-				lineStyle: {
-					color: '#38b8ff'
-				}
-			},
-			axisLabel: {
-				color: '#fff',
-				fontSize: 10
-			}
-		}],
-		yAxis: [
-			{
-				name:'',
-				nameGap:-5,
-				nameTextStyle:{
-					padding:[0,0,0,25],
-					align:'center',
-					color:'#fff',
-				},
-				axisLine: {
-					lineStyle: {
-						color: '#38b8ff'
-					}
-				},
-				axisLabel: {
-					color: '#fff',
-					fontSize: 10,
-				},
-				splitLine: {
-					show:false,
-					lineStyle: {
-						color: '#0177d4'
-					}
-				}
-			},{
-				nameTextStyle: {
-					color: '#fff',
-					fontSize: 10
-				},
-				axisLine: {
-					lineStyle: {
-						color: '#38b8ff'
-					}
-				},
-				axisLabel: {
-					color: '#fff',
-					fontSize: 10,
-					formatter: '{value}%'
-				},
-				splitLine: {
-					show:false,
-					lineStyle: {
-						color: '#0177d4'
-					}
-				}
-			}],
-		color:['#FFD743','#2AB7FF','#30FFFE','#216FFF'],
-		series: [
-			{
-				name: "低碳科技支出",
-				type: "bar",
-				barWidth: '20%',
-				itemStyle: {
-					normal: {
-						barBorderRadius: 10,
-					},
-				},
-				data: data[2],
-			},
-			{
-				name: "总支出",
-				type: "bar",
-				barWidth: '20%',
-				itemStyle: {
-					normal: {
-						barBorderRadius: 10,
-					},
-				},
-				data: data[3],
-			},
-			{
-				name: "占比",
-				type: "line",
-				yAxisIndex: 1,
-				itemStyle: {
-					normal: {
-					},
-				},
-				data: data[4],
-			},
-		]
-	};
-	$chart.init('#chart3', option);
-}
-//低碳技术研发人员与产业人员占比
-function chart4(data){
-	var option =  {
-		legend: {
-			data: data[0],
-		},
-		grid: {
-			left: '5%',
-			right:'5%',
-			top:'10%',
-			bottom:'18%',
-			containLabel: true
-		},
-		tooltip: {
-			trigger: 'axis',
-			axisPointer: { // 坐标轴指示器，坐标轴触发有效
-				type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-			}
-		},
-		xAxis: [{
-			type: 'category',
-			gridIndex: 0,
-			data: data[1],
-			axisLine: {
-				lineStyle: {
-					color: '#38b8ff'
-				}
-			},
-			axisLabel: {
-				color: '#fff',
-				fontSize: 10
-			}
-		}],
-		yAxis: [
-			{
-				name:'',
-				nameGap:-5,
-				nameTextStyle:{
-					padding:[0,0,0,25],
-					align:'center',
-					color:'#fff',
-				},
-				axisLine: {
-					lineStyle: {
-						color: '#38b8ff'
-					}
-				},
-				axisLabel: {
-					color: '#fff',
-					fontSize: 10,
-				},
-				splitLine: {
-					show:false,
-					lineStyle: {
-						color: '#0177d4'
-					}
-				}
-			},{
-				nameTextStyle: {
-					color: '#fff',
-					fontSize: 10
-				},
-				axisLine: {
-					lineStyle: {
-						color: '#38b8ff'
-					}
-				},
-				axisLabel: {
-					color: '#fff',
-					fontSize: 10,
-					formatter: '{value}%'
-				},
-				splitLine: {
-					show:false,
-					lineStyle: {
-						color: '#0177d4'
-					}
-				}
-			}],
-		series: [
-			{
-				name: "低碳技术研发人员",
-				type: "bar",
-				barWidth: '20%',
-				itemStyle: {
-					normal: {
-						color: '#2c18f3',
-						barBorderRadius: 10,
-					},
-				},
-				data: data[2],
-			},
-			{
-				name: "产业人员",
-				type: "bar",
-				barWidth: '20%',
-				itemStyle: {
-					normal: {
-						color: '#30fffe',
-						barBorderRadius: 10,
-					},
-				},
-				data: data[3],
-			},
-			{
-				name: "占比",
-				type: "line",
-				yAxisIndex: 1,
-				itemStyle: {
-					normal: {
-						color: '#E9DC37',
-					},
-				},
-				data: data[4],
-			},
-		]
-	};
 	$chart.init('#chart4', option);
 }
 //低碳技术经费投入情况
-function chart5(data){
+function chart5(data, ix){
 	var option =  {
 		legend: {
-			data: data[0],
+			data: data.legend,
 		},
 		color: ['#6ed5ff','#ff3a83','#2874ff','#ffa24c','#af59ff'],
 		grid: {
@@ -591,7 +581,7 @@ function chart5(data){
 		xAxis: [{
 			type: 'category',
 			gridIndex: 0,
-			data: data[1],
+			data: data.xData,
 			axisLine: {
 				lineStyle: {
 					color: '#38b8ff'
@@ -604,7 +594,7 @@ function chart5(data){
 		}],
 		yAxis: [
 			{
-				name:'万千瓦时',
+				name:'',
 				nameGap:-5,
 				nameTextStyle:{
 					padding:[0,0,0,45],
@@ -650,45 +640,15 @@ function chart5(data){
 			}],
 		series: [
 			{
-				name: "CODcr产生量",
+				name: "技术经费投入占比",
 				type: "bar",
+				barWidth:"30%",
 				itemStyle: {
 					normal: {
-						barBorderRadius: 50,
+
 					},
 				},
-				data: data[2],
-			},
-			{
-				name: "挥发酚产生量",
-				type: "bar",
-				itemStyle: {
-					normal: {
-						barBorderRadius: 50,
-					},
-				},
-				data: data[3],
-			},
-			{
-				name: "石油类污染物产生量",
-				type: "bar",
-				itemStyle: {
-					normal: {
-						barBorderRadius: 50,
-					},
-				},
-				data: data[4],
-			},
-			{
-				name: "VOC总量削减率",
-				type: "line",
-				yAxisIndex: 1,
-				itemStyle: {
-					normal: {
-						color: '#e7572d'
-					},
-				},
-				data: data[5],
+				data: data.data[ix][0],
 			},
 		]
 	};
